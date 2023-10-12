c1ba673c82401e2314be1a64d11a5c19c2a52449
OolongTea620 <rlafls9596@gmail.com>
2023-10-12 17:00:36 +0900

    .jar chmod 권한 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,8 @@ ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
+## .jar 권한 부여
+chmod 755 $PROJECT_ROOT/build/libs/*.jar
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
