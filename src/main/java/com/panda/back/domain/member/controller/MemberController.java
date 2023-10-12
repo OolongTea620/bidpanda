bddd98fbb66ec08e59d96298884217e00cd81c4b
OolongTea620 <rlafls9596@gmail.com>
2023-10-12 13:19:36 +0900

    appspec 수정, build.gradle 설정 추가

--- src/main/java/com/panda/back/domain/member/controller/MemberController.java
+++ src/main/java/com/panda/back/domain/member/controller/MemberController.java
@@ -38,6 +38,11 @@ public class MemberController {
     private final KakaoService kakaoService;
     private final MailSerivce mailSerivce;
 
+    @GetMapping("/ping")
+    public String hello() {
+        return "pong";
+    }
+
     @Operation(summary = "아이디 중복 체크")
     @GetMapping("/membername/{membername}")
     public ResponseEntity<BaseResponse> membernameExists(@PathVariable String membername) {
